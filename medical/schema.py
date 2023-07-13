47d0cd2d5362e9b1b6e375b9d42e0982f85861d9
Tiah Kevel <k.choumene@cloudfacile.com>
2023-07-13 15:33:17 +0100

    main actuel imis

--- medical/schema.py
+++ medical/schema.py
@@ -9,11 +9,11 @@ from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from medical.gql_mutations import CreateServiceMutation, UpdateServiceMutation, DeleteServiceMutation, \
     CreateItemMutation, UpdateItemMutation, DeleteItemMutation
-from .gql_queries import *
 
 from .apps import MedicalConfig
 from .models import Diagnosis, Item, Service
 import graphene_django_optimizer as gql_optimizer
+from .services import check_unique_code_item, check_unique_code_service
 
 
 class DiagnosisGQLType(DjangoObjectType):
@@ -38,6 +38,21 @@ class ItemGQLType(DjangoObjectType):
             'name': ['exact', 'icontains', 'istartswith'],
             'package': ['exact', 'icontains', 'istartswith'],
             'type': ['exact'],
+        }
+        connection_class = ExtendedConnection
+
+
+class ServiceGQLType(DjangoObjectType):
+    class Meta:
+        model = Service
+        interfaces = (graphene.relay.Node,)
+        filter_fields = {
+            'uuid': ['exact'],
+            'code': ['exact', 'icontains', 'istartswith'],
+            'name': ['exact', 'icontains', 'istartswith'],
+            'type': ['exact'],
+            'care_type': ['exact'],
+            'category': ['exact'],
         }
         connection_class = ExtendedConnection
 
@@ -77,6 +92,16 @@ class Query(graphene.ObjectType):
         orderBy=graphene.List(of_type=graphene.String),
         pricelist_uuid=graphene.UUID(),
     )
+    validate_item_code = graphene.Field(
+        graphene.Boolean,
+        item_code=graphene.String(required=True),
+        description="Checks that the specified item code is unique."
+    )
+    validate_service_code = graphene.Field(
+        graphene.Boolean,
+        service_code=graphene.String(required=True),
+        description="Checks that the specified service code is unique."
+    )
 
     def resolve_diagnoses_str(self, info, **kwargs):
         if not info.context.user.has_perms(MedicalConfig.gql_query_diagnosis_perms):
@@ -97,7 +122,8 @@ class Query(graphene.ObjectType):
         search_str = kwargs.get("str")
         q = Item.objects.filter(*filter_validity(date))
         if pricelist_uuid is not None:
-            q = q.filter(pricelist_details__items_pricelist__uuid=pricelist_uuid)
+            q = q.filter(pricelist_details__items_pricelist__uuid=pricelist_uuid,
+                         pricelist_details__validity_to__isnull=True)
         if search_str is not None:
             q = q.filter(Q(code__icontains=search_str) | Q(name__icontains=search_str))
         return q
@@ -140,7 +166,8 @@ class Query(graphene.ObjectType):
         search_str = kwargs.get("str")
         q = Service.objects.filter(*filter_validity(date))
         if pricelist_uuid is not None:
-            q = q.filter(pricelist_details__services_pricelist__uuid=pricelist_uuid)
+            q = q.filter(pricelist_details__services_pricelist__uuid=pricelist_uuid,
+                         pricelist_details__validity_to__isnull=True)
         if search_str is not None:
             q = q.filter(Q(code__icontains=search_str) | Q(name__icontains=search_str))
         return q
@@ -171,6 +198,18 @@ class Query(graphene.ObjectType):
         if not show_history:
             queryset = queryset.filter(*filter_validity(**kwargs))
         return gql_optimizer.query(queryset, info)
+
+    def resolve_validate_service_code(self, info, **kwargs):
+        if not info.context.user.has_perms(MedicalConfig.gql_query_medical_services_perms):
+            raise PermissionDenied(_("unauthorized"))
+        errors = check_unique_code_service(code=kwargs['service_code'])
+        return False if errors else True
+
+    def resolve_validate_item_code(self, info, **kwargs):
+        if not info.context.user.has_perms(MedicalConfig.gql_query_medical_items_perms):
+            raise PermissionDenied(_("unauthorized"))
+        errors = check_unique_code_item(code=kwargs['item_code'])
+        return False if errors else True
 
 
 class Mutation(graphene.ObjectType):
